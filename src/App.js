2da30dd61d8955b37bcfff3db3bca0c9f9933dbf
cam-ball <hi@camball.me>
2023-01-16 00:35:20 -0500

    create-react-app, get the table in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './constants.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Roll</th>
+            <th>Adjective</th>
+            <th>Primary Descriptor</th>
+            <th>Quirk</th>
+          </tr>
+        </thead>
+      <tbody>
+        <TableData />
+      </tbody>
+      </table>
     </div>
   );
+}
+
+const TableData = () => {
+  const tableRows = [];
+  for (let i = 0; i < 20; i++) {
+    tableRows.push(
+      <tr key={i+1}>
+        <td>{i+1}</td>
+        <td>{data.adjectives[i]}</td>
+        <td>{data.descriptors[i]}</td>
+        <td>{data.quirks[i]}</td>
+      </tr>
+    )
+  }
+  return tableRows
 }
 
 export default App;
